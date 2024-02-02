bf6b5a849073f832964281fee6824847844f7ad7
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2024-02-02 10:26:29 +0200

    changed the headername

--- sociopediaADMINBOARD/src/Sidebar.jsx
+++ sociopediaADMINBOARD/src/Sidebar.jsx
@@ -39,7 +39,7 @@ function Sidebar({ openSidebarToggle, OpenSidebar }) {
     <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive" : ""}>
       <div className='sidebar-title'>
         <div className='sidebar-brand'>
-          <BsCart3 className='icon_header' /> SHOP
+          <BsCart3 className='icon_header' /> SOCIOPEDIA
         </div>
         <span className='icon close_icon' onClick={OpenSidebar}>X</span>
       </div>
