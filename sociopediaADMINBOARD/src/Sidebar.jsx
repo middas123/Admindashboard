b44ab6e27c3c86a009cdd8e765d2c17be836ae23
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2024-02-03 19:56:48 +0200

    middas

--- sociopediaADMINBOARD/src/Sidebar.jsx
+++ sociopediaADMINBOARD/src/Sidebar.jsx
@@ -77,7 +77,7 @@ function Sidebar({ openSidebarToggle, OpenSidebar }) {
         </li>
         <li className={`sidebar-list-item ${activeButton === 'settings' ? 'active' : ''}`}>
           <a href="#" onClick={(e) => handleButtonClick('settings', e)}>
-            <BsFillGearFill className='icon' /> 
+            <BsFillGearFill className='icon' /> Settings
           </a>
         </li>
       </ul>
